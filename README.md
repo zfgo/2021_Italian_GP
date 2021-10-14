4beb346bda035493a15fdf7eee1cce0c9f2d69e3
Boromir-the-Brave <zanegloh@gmail.com>
2021-10-14 00:10:49 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 2021 Italian Grand Prix: RIC vs VER in stint 1
+
+This code was shamelessly stolen from [this](https://medium.com/@jaspervhat/how-i-analyze-formula-1-data-with-python-2021-italian-gp-dfb11db4b73) blog.
+
+This is mostly just a test by me to make sure that I have plenty of examples for future F1 telemetry projects that I hope to do with the `fastf1` python package.
---
+++ main.py
@@ -0,0 +1,91 @@
+
+import fastf1 as ff1
+from fastf1 import plotting
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import figure
+import numpy as np
+import pandas as pd
+
+# Setup plotting
+plotting.setup_mpl()
+
+# Enable the cache
+path_file = "path.txt"
+
+with open(path_file, 'r') as f:
+    path = f.read()
+
+# path = <INSERT YOUR CACHE PATH HERE>
+
+ff1.Cache.enable_cache(cache_dir=path,
+                       ignore_version=False,
+                       force_renew=False,
+                       use_requests_cache=True)
+
+# Get rid of some pandas warnings that are not relevant for us at the moment
+pd.options.mode.chained_assignment = None
+
+# Load the session data
+race = ff1.get_session(2021, 'Monza', 'R')
+
+# Get the laps
+laps = race.load_laps(with_telemetry=True)
+
+# Get laps of the drivers (BOT and HAM)
+laps_ric = laps.pick_driver('RIC')
+laps_ver = laps.pick_driver('VER')
+
+
+# We are only analyzing stint 1, so select that one
+laps_ric = laps_ric.loc[laps_ric['Stint'] == 1]
+laps_ver = laps_ver.loc[laps_ver['Stint'] == 1]
+
+laps_ric['RaceLapNumber'] = laps_ric['LapNumber'] - 1
+laps_ver['RaceLapNumber'] = laps_ver['LapNumber'] - 1
+
+full_distance_ver_ric = pd.DataFrame()
+summarized_distance_ver_ric = pd.DataFrame()
+
+for lap in laps_ver.iterlaps():
+    telemetry = lap[1].get_car_data().add_distance().add_driver_ahead()
+
+    # Only run this loop when driver ahead is RIC, otherwise we compare wrong distance gaps
+    telemetry = telemetry.loc[telemetry['DriverAhead'] == "3"]
+
+    if len(telemetry) != 0:
+        # Full distance
+        lap_telemetry = telemetry[['Distance', 'DistanceToDriverAhead']]
+        lap_telemetry.loc[:, 'Lap'] = lap[0] + 1
+
+        full_distance_ver_ric = full_distance_ver_ric.append(lap_telemetry)
+
+        # Average / median distance
+        distance_mean = np.nanmean(telemetry['DistanceToDriverAhead'])
+        distance_median = np.nanmedian(telemetry['DistanceToDriverAhead'])
+
+        summarized_distance_ver_ric = summarized_distance_ver_ric.append({
+            'Lap': lap[0] + 1,
+            'Mean': distance_mean,
+            'Median': distance_median
+        }, ignore_index=True)
+
+plt.rcParams['figure.figsize'] = [10, 6]
+
+fig, ax = plt.subplots(2)
+fig.suptitle("RIC vs VER opening stint comparison")
+
+ax[0].plot(laps_ric['RaceLapNumber'], laps_ric['LapTime'], label='RIC')
+ax[0].plot(laps_ver['RaceLapNumber'], laps_ver['LapTime'], label='VER')
+ax[0].set(ylabel='Laptime', xlabel='Lap')
+ax[0].legend(loc="upper center")
+
+ax[1].plot(summarized_distance_ver_ric['Lap'], summarized_distance_ver_ric['Mean'], label='Mean', color='red')
+ax[1].plot(summarized_distance_ver_ric['Lap'], summarized_distance_ver_ric['Median'], label='Median', color='grey')
+ax[1].set(ylabel='Distance (meters)', xlabel='Lap')
+ax[1].legend(loc="upper center")
+
+# Hide x labels and tick labels for top plots and y ticks for right plots.
+for a in ax.flat:
+    a.label_outer()
+
+plt.show()
